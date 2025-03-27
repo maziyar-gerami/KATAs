0a69a8d9c5181373c3fd895faa5470bb1c78f7d4
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2025-03-27 12:16:44 +0330

    Palindrome : #1 : Add failure test

--- Plaindrome/#1/test/org/palindrome/PalindromeTest.java
+++ Plaindrome/#1/test/org/palindrome/PalindromeTest.java
@@ -2,21 +2,29 @@ package org.palindrome;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class PalindromeTest {
 
     @Test
-    public void givenOddString_whenIsPalindrome_thenReturnTrue(){
+    public void givenOddString_whenIsPalindrome_thenReturnTrue() {
         String s = "Madam";
         boolean result = Palindrome.calculate(s);
         assertTrue(result);
     }
 
     @Test
-    public void givenEvenString_whenIsPalindrome_thenReturnTrue(){
+    public void givenEvenString_whenIsPalindrome_thenReturnTrue() {
         String s = "Maam";
         boolean result = Palindrome.calculate(s);
         assertTrue(result);
+    }
+
+    @Test
+    public void givenOddString_whenIsNotPalindrome_thenReturnFalse() {
+        String s = "Palindrome";
+        boolean result = Palindrome.calculate(s);
+        assertFalse(result);
     }
 }
