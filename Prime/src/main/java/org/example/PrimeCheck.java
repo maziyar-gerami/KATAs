3dfe9e3a603a4c6a7ec448da97e5217936e63fe4
Alirez-13 <alirezaholghi@gmail.com>
2025-04-15 13:09:39 -0400

    Class name changed

--- Prime/src/main/java/org/example/PrimeCheck.java
+++ Prime/src/main/java/org/example/PrimeCheck.java
@@ -1,16 +1,18 @@
 package org.example;
 
-public class PrimeChecker {
+public class PrimeCheck {
 
     public boolean isPrime(int input) {
+
         double squared = Math.sqrt(input);
+
         if (input < 2)
             return false;
 
-        for (int i = 2; i <= squared; i++) {
+        for (int i = 2; i <= squared; i++)
             if (input % i == 0)
                 return false;
-        }
+
         return true;
     }
 }
