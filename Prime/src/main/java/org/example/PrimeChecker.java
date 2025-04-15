dc2114a0fbbf8378bfb2e1d1cb9c9dfea2541a9c
Alirez-13 <alirezaholghi@gmail.com>
2025-04-15 13:08:03 -0400

    remove braces

--- Prime/src/main/java/org/example/PrimeChecker.java
+++ Prime/src/main/java/org/example/PrimeChecker.java
@@ -4,12 +4,12 @@ public class PrimeChecker {
 
     public boolean isPrime(int input) {
         double squared = Math.sqrt(input);
-        if (input < 2) return false;
+        if (input < 2)
+            return false;
 
         for (int i = 2; i <= squared; i++) {
-            if (input % i == 0) {
+            if (input % i == 0)
                 return false;
-            }
         }
         return true;
     }
