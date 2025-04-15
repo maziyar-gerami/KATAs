af19ad590983187cc75e24e8ef031dad73808919
mehr64 <kashfimehrnaz85@gmail.com>
2025-04-15 13:38:23 +0100

    Move detector code into PrimeDetector folders

--- PrimeDetector/src/DetectPrimeNumbers.java
+++ PrimeDetector/src/DetectPrimeNumbers.java
@@ -1,3 +1,15 @@
+import static java.lang.Math.*;
+
 public class DetectPrimeNumbers {
 
+    public static boolean detector(int input) {
+        if (input < 2)
+            return false;
+        double middleOfInput = (int) sqrt(input);
+        for (int i = 2; i <= middleOfInput; i++) {
+            if (input % i == 0)
+                return false;
+        }
+        return true;
+    }
 }
--- PrimeDetector/test/DetectPrimeNumbersTest.java
+++ PrimeDetector/test/DetectPrimeNumbersTest.java
@@ -1,3 +1,25 @@
 public class DetectPrimeNumbersTest {
+    
+    @Test
+    public void testDetector_withPrimeNumbers(){
+        assertTrue(DetectPrimeNumbers.detector(11));
+    }
+    @Test
+    public void testDetector_withNotPrimeNumbers(){
+        assertFalse(DetectPrimeNumbers.detector(9));
+    }
+    @Test
+    public void testDetector_withEdgeCase1() {
+        assertFalse(DetectPrimeNumbers.detector(1));
+    }
 
+    @Test
+    public void testDetector_withZero() {
+        assertFalse(DetectPrimeNumbers.detector(0));
+    }
+
+    @Test
+    public void testDetector_withNegative() {
+        assertFalse(DetectPrimeNumbers.detector(-5));
+    }
 }
