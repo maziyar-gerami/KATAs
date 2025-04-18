122c10ae04739c78205b40c0822d628d9d825826
Alirez-13 <alirezaholghi@gmail.com>
2025-04-18 10:54:59 -0400

    some minor changes in isPrime

--- Prime/src/main/java/org/example/PrimeCheck.java
+++ Prime/src/main/java/org/example/PrimeCheck.java
@@ -3,11 +3,10 @@ package org.example;
 public class PrimeCheck {
 
     public boolean isPrime(int input) {
-
-        double squared = Math.sqrt(input);
-
         if (input < 2)
             return false;
+
+        double squared = Math.sqrt(input);
 
         for (int i = 2; i <= squared; i++)
             if (input % i == 0)
