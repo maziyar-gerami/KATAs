6ff20a2cc6b58fb9985dd3eff18fe06de26ec309
Alirez-13 <alirezaholghi@gmail.com>
2025-04-19 05:49:28 -0400

    The test was written

--- Balancer/src/main/java/org/example/Balancer.java
+++ Balancer/src/main/java/org/example/Balancer.java
@@ -2,10 +2,13 @@ package org.example;
 
 public class Balancer {
 
-    public static boolean isBalance(String input){
+    public static boolean isBalance(String input) {
         if (input.isEmpty())
             return false;
 
+        for (int i = 0; i < input.length() - 1; i++) {
+
+        }
 
         return false;
     }
