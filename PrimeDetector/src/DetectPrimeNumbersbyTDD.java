16f001a8e43bde724bf5f4b181c07e3af3127062
mehr64 <kashfimehrnaz85@gmail.com>
2025-04-23 16:50:59 +0100

    DetectPrimeNumbersbyTDD: 1Refactor

--- PrimeDetector/src/DetectPrimeNumbersbyTDD.java
+++ PrimeDetector/src/DetectPrimeNumbersbyTDD.java
@@ -1,7 +1,10 @@
 public class DetectPrimeNumbersbyTDD {
 
-
     public static boolean detector(int i) {
-        return True;
+        double middleOfInput = (int) sqrt(input);
+        for (int i = 2; i <= middleOfInput; i++)
+            if (input % i == 0)
+                return false;
+        return true;
     }
 }
