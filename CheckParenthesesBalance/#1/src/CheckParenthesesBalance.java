c21d59443331ee051e8be2df32663ef369de14f9
mehr64 <kashfimehrnaz85@gmail.com>
2025-04-25 13:21:19 +0100

    CPB: 2Refacotr

--- CheckParenthesesBalance/#1/src/CheckParenthesesBalance.java
+++ CheckParenthesesBalance/#1/src/CheckParenthesesBalance.java
@@ -1,6 +1,28 @@
+import java.util.Stack;
+
 public class CheckParenthesesBalance {
 
     public static boolean BalanceChecker(String input) {
-        return input.equals("()") || input.equals("");
+        Stack<Character> stack = new Stack<>();
+        for (char ch : input.toCharArray()) {
+            if (ch == '(' || ch == '[' || ch == '{') {
+                stack.push(ch);
+            } else if (ch == ')' || ch == ']' || ch == '}') {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                char open = stack.pop();
+                if (!matches(open, ch)) {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    private static boolean matches(char open, char close) {
+        return (open == '(' && close == ')') ||
+                (open == '[' && close == ']') ||
+                (open == '{' && close == '}');
     }
 }
