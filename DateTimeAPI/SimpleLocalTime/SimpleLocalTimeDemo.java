29338a7b179d64ba8e89badfcd5cc9f3468c618f
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2025-12-20 19:01:22 +0330

    Add SimpleLocalTimeDemo

---
+++ DateTimeAPI/SimpleLocalTime/SimpleLocalTimeDemo.java
@@ -0,0 +1,11 @@
+import java.time.LocalTime;
+
+public class SimpleLocalTimeDemo {
+    
+    public static void main(String[] args) {
+        
+    LocalTime lt = LocalTime.now();
+    System.out.print(lt.toString());
+
+    }
+}
