15e88e1f1438ff1c25d0035d189a0d3574a37802
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2025-12-20 18:33:13 +0330

    Add SimpleLocalDateDemo to test SimpleLocalDate

---
+++ DateTimeAPI/Simpe-1/SimpleLocalDateDemo.java
@@ -0,0 +1,9 @@
+import java.time.LocalDate;
+
+public class SimpleLocalDateDemo {
+    
+    public static void main(String[] args) {
+        var ld = LocalDate.now();
+        System.out.println(ld.toString());
+    }
+}
