bd17233884aa9f04a937cb16a615122bbc5b650d
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2026-05-28 09:51:49 +0330

    Complete

--- CharacterFrequencyCounter/src/main/java/org/example/CharacterFrequencyCounter.java
+++ CharacterFrequencyCounter/src/main/java/org/example/CharacterFrequencyCounter.java
@@ -1,4 +1,29 @@
 package org.example;
 
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class CharacterFrequencyCounter {
+
+    public static Map<Character, Integer> countCharacters(String str) {
+        if (str == null || str.isBlank())
+            return new LinkedHashMap<>();
+
+        return str
+                .chars()
+                .mapToObj(c -> (char) c)
+                .filter(c -> !Character.isWhitespace(c))
+                .collect(Collectors.toMap(
+                        Function.identity(),
+                        c -> 1,
+                        Integer::sum,
+                        LinkedHashMap::new
+                ));
+    }
 }
--- CharacterFrequencyCounter/src/test/java/org/example/CharacterFrequencyCounterTest.java
+++ CharacterFrequencyCounter/src/test/java/org/example/CharacterFrequencyCounterTest.java
@@ -1,7 +1,34 @@
 package org.example;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class CharacterFrequencyCounterTest {
+
+    @Test
+    void counter_whenTextIsSimple_countIt() {
+        String str = "Hello World! 22";
+        var expectedResult = new LinkedHashMap<>(
+                Map.of('!', 1, '2', 2, 'd', 1, 'e', 1, 'H', 1, 'l', 3, 'o', 2, 'r', 1, 'W', 1));
+
+        var result = CharacterFrequencyCounter.countCharacters(str);
+
+        assertThat(result).isEqualTo(expectedResult);
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    void counter_whenTextIsNullOrEmpty(String str){
+        var result = CharacterFrequencyCounter.countCharacters(str);
+
+        assertThat(result).isEmpty();
+    }
 
 }
