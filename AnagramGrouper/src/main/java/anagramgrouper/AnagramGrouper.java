a808cd57caf8466438767b4e17d31965a6025c3c
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2026-06-05 15:27:02 +0330

    AnagramGrouper: Implement grouping logic and add tests

--- AnagramGrouper/src/main/java/anagramgrouper/AnagramGrouper.java
+++ AnagramGrouper/src/main/java/anagramgrouper/AnagramGrouper.java
@@ -1,11 +1,34 @@
 package anagramgrouper;
 
-import java.util.List;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 
+import java.util.*;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class AnagramGrouper {
 
-    public static List<List<String>> group(List<String> words){
-        return null;
+    public static List<List<String>> group(List<String> words) {
+        if (words == null || words.isEmpty())
+            return List.of();
 
+        Map<String, ArrayList<String>> result = new HashMap<>();
+
+        for (String word : words) {
+            char[] chars = word.toLowerCase().toCharArray();
+            Arrays.sort(chars);
+            String processedWord = new String(chars);
+
+            List<String> groupedWords = result.get(processedWord);
+            if (groupedWords == null)
+                result.put(processedWord, new ArrayList<>(List.of(word)));
+            else
+                groupedWords.add(word);
+        }
+
+        return result.values()
+                .stream()
+                .map(list -> (List<String>) list)
+                .toList();
     }
 }
--- AnagramGrouper/src/test/java/anagramgrouper/AnagramGrouperTest.java
+++ AnagramGrouper/src/test/java/anagramgrouper/AnagramGrouperTest.java
@@ -1,7 +1,38 @@
 package anagramgrouper;
 
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+
+import java.util.List;
+import java.util.stream.Stream;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+
 class AnagramGrouperTest {
+
+    public static Stream<Arguments> wordsProvider() {
+        return Stream.of(Arguments.of(List.of("Listen"), List.of(List.of("Listen"))),
+                Arguments.of(List.of("Listen", "Eat"), List.of(List.of("Eat"), List.of("Listen"))),
+                Arguments.of(List.of("Listen", "Eat", "Silent"), List.of(List.of("Eat"), List.of("Listen", "Silent"))),
+                Arguments.of(List.of("Listen", "Eat", "Tea", "Silent"), List.of(List.of("Eat", "Tea"), List.of("Listen", "Silent"))));
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    void angramGrouper_whenWordsListIsEmpty_returnEmptyList(List<String> words) {
+        var result = AnagramGrouper.group(words);
+        assert result != null;
+        assertTrue (result.isEmpty());
+    }
+
+    @ParameterizedTest
+    @MethodSource("wordsProvider")
+    void angramGrouper_whenValidWords_returnNgramGrouped(List<String> words, List<List<String>> expectedResult) {
+        var result = AnagramGrouper.group(words);
+        assertEquals(expectedResult, result);
+    }
 
 }
