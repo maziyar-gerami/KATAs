d4df58b75f71c40c8de1fafd84d1164dd23fb23a
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2026-06-06 15:44:05 +0330

    WordLengthStatistics: Implement word length statistics calculation and tests

--- WordLengthStatistics/src/main/java/wordlengthstatistics/WordLengthStatistics.java
+++ WordLengthStatistics/src/main/java/wordlengthstatistics/WordLengthStatistics.java
@@ -1,10 +1,24 @@
 package wordlengthstatistics;
 
-import java.util.Map;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 
+import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class WordLengthStatistics {
 
-    public static Map<Integer, Long> calculate(String text){
-        return null;
+    public static Map<Integer, Long> calculate(String text) {
+        if (text == null || text.isBlank())
+            return Map.of();
+
+        var words = text
+                .replaceAll(" +", " ")
+                .strip().split(" ");
+
+        return Arrays.stream(words)
+                .collect(Collectors.groupingBy(String::length, Collectors.counting()));
     }
 }
--- WordLengthStatistics/src/test/java/wordlengthstatistics/WordLengthStatisticsTest.java
+++ WordLengthStatistics/src/test/java/wordlengthstatistics/WordLengthStatisticsTest.java
@@ -1,7 +1,43 @@
 package wordlengthstatistics;
 
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Stream;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class WordLengthStatisticsTest {
 
+    public static Stream<Arguments> sentenceAndCountProvider() {
+        return Stream.of(Arguments.of("This is java", new HashMap<>(Map.of(4, 2L, 2, 1L))),
+                Arguments.of("   This  is java an    strong   language   ", new HashMap<>(Map.of(2, 2L, 4, 2L, 6, 1L, 8, 1L))));
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    @ValueSource(strings = {"", " ", "      "})
+    void wordLengthStatistics_whenSentenceIsEmpty_returnEmptyMap(String sentence){
+        var result = WordLengthStatistics.calculate(sentence);
+        assertEquals(Map.of(), result);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"Maziyar", " maziyar ", "   maziyar   "})
+    void wordLengthStatistics_whenSentenceIsOneWord_returnEmptyMap(String sentence){
+        var result = WordLengthStatistics.calculate(sentence);
+        assertEquals(Map.of(7, 1L), result);
+    }
+
+    @ParameterizedTest
+    @MethodSource(value = "sentenceAndCountProvider")
+    void wordLengthStatistics_whenSentenceHasMoreWords_returnCountMap(String sentence, HashMap<Integer, Long> wordsCount){
+        var result = WordLengthStatistics.calculate(sentence);
+        assertEquals(wordsCount, result);
+    }
 }
