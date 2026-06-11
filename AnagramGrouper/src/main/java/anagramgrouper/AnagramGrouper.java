29afccb241f393bf591fe740e8ab40873d51ed12
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2026-06-11 09:47:42 +0330

    AnagramGrouper: Empty Response

--- AnagramGrouper/src/main/java/anagramgrouper/AnagramGrouper.java
+++ AnagramGrouper/src/main/java/anagramgrouper/AnagramGrouper.java
@@ -9,26 +9,7 @@ import java.util.*;
 public class AnagramGrouper {
 
     public static List<List<String>> group(List<String> words) {
-        if (words == null || words.isEmpty())
-            return List.of();
 
-        Map<String, ArrayList<String>> result = new HashMap<>();
-
-        for (String word : words) {
-            char[] chars = word.toLowerCase().toCharArray();
-            Arrays.sort(chars);
-            String processedWord = new String(chars);
-
-            List<String> groupedWords = result.get(processedWord);
-            if (groupedWords == null)
-                result.put(processedWord, new ArrayList<>(List.of(word)));
-            else
-                groupedWords.add(word);
-        }
-
-        return result.values()
-                .stream()
-                .map(list -> (List<String>) list)
-                .toList();
+        return null;
     }
 }
--- AnagramGrouper/src/test/java/anagramgrouper/AnagramGrouperTest.java
+++ AnagramGrouper/src/test/java/anagramgrouper/AnagramGrouperTest.java
@@ -10,29 +10,28 @@ import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-
 class AnagramGrouperTest {
-
-    public static Stream<Arguments> wordsProvider() {
-        return Stream.of(Arguments.of(List.of("Listen"), List.of(List.of("Listen"))),
-                Arguments.of(List.of("Listen", "Eat"), List.of(List.of("Eat"), List.of("Listen"))),
-                Arguments.of(List.of("Listen", "Eat", "Silent"), List.of(List.of("Eat"), List.of("Listen", "Silent"))),
-                Arguments.of(List.of("Listen", "Eat", "Tea", "Silent"), List.of(List.of("Eat", "Tea"), List.of("Listen", "Silent"))));
-    }
 
     @ParameterizedTest
     @NullAndEmptySource
     void angramGrouper_whenWordsListIsEmpty_returnEmptyList(List<String> words) {
         var result = AnagramGrouper.group(words);
         assert result != null;
-        assertTrue (result.isEmpty());
+        assertTrue(result.isEmpty());
     }
 
     @ParameterizedTest
     @MethodSource("wordsProvider")
-    void angramGrouper_whenValidWords_returnNgramGrouped(List<String> words, List<List<String>> expectedResult) {
+    void angramGrouper_whenValidWords_returnAngramGrouped(List<String> words, List<List<String>> expectedResult) {
         var result = AnagramGrouper.group(words);
         assertEquals(expectedResult, result);
+    }
+
+    public static Stream<Arguments> wordsProvider() {
+        return Stream.of(Arguments.of(List.of("Listen"), List.of(List.of("Listen"))),
+                Arguments.of(List.of("Listen", "Eat"), List.of(List.of("Eat"), List.of("Listen"))),
+                Arguments.of(List.of("Listen", "Eat", "Silent"), List.of(List.of("Eat"), List.of("Listen", "Silent"))),
+                Arguments.of(List.of("Listen", "Eat", "Tea", "Silent"), List.of(List.of("Eat", "Tea"), List.of("Listen", "Silent"))));
     }
 
 }
